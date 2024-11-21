c8245059364276828f7b18f1988cce710960948a
Abdelrhman Nile <abdelrhmannile@gmail.com>
2024-11-21 14:08:58 +0200

    fix loading trained weights

--- inference_script.py
+++ inference_script.py
@@ -8,6 +8,7 @@ from torchvision import transforms
 from datasets.mvtec import MVTecDataset
 from datasets.mvtec import DatasetSplit
 import cv2
+import os
 
 
 test_dataset = MVTecDataset(
@@ -28,8 +29,6 @@ test_dataloader = torch.utils.data.DataLoader(
                 pin_memory=True,
             )
 
-IMAGENET_MEAN = [0.485, 0.456, 0.406]
-IMAGENET_STD = [0.229, 0.224, 0.225]
 
 backbone = backbones.load("wideresnet50")
 layers_to_extract = ["layer2", "layer3"]
@@ -37,7 +36,7 @@ device = utils.set_torch_device([0])
 input_size = (3, 288, 288)
 pretrain_dim = 1536
 target_dim = 1536
-
+pre_proj = 1
 model = SimpleNet(device=device)
 model.load(
     backbone=backbone,
@@ -46,6 +45,8 @@ model.load(
     input_shape=input_size,
     pretrain_embed_dimension=pretrain_dim,
     target_embed_dimension=target_dim,
+    pre_proj=pre_proj,
+
 )
 
 
@@ -59,7 +60,18 @@ img = transform_img(img)
 #make batch of 1
 img = img.unsqueeze(0) """
 
-#print(model.test(test_data=test_dataloader, save_segmentation_images=True, ckpt_path="/content/ckpt.pth"))
+state_dict = {}
+ckpt_path = "/content/screw_ckpt.pth"
+state_dict = torch.load(ckpt_path, map_location=model.device)
+if 'discriminator' in state_dict:
+    print("Loading discriminator weights")
+    model.discriminator.load_state_dict(state_dict['discriminator'])
+    if "pre_projection" in state_dict:
+        print("Loading pre_projection weights")
+        model.pre_projection.load_state_dict(state_dict["pre_projection"])
+else:
+    print("Loading model weights")
+    model.load_state_dict(state_dict, strict=False)
 
 
 scores, masks, features, labels_gt, masks_gt = model.predict(test_dataloader)
